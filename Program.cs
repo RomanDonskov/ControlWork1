1a500cfb7b894da557b2560a25f0e39b6bafb6bb
Orix <orix_send@mail.ru>
2023-04-15 13:26:23 +0300

    Написана программа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа выводит массив из строк, состоящих из элементов исходного текстового массива, длина которых меньше или равна 3
+Console.Clear();
+
+string[] text = new string[]  {"1", "22", "333", "4444", "abc", "Fhkljh", "Rtewne"};
+Console.WriteLine("Исходный массив:");
+Console.Write($"[{string.Join("  ", text)}]");
+Console.WriteLine();
+Console.WriteLine("Элементы, длина которых меньше или равна 3:");
+Console.Write($"[{string.Join("  ", GetArraySpecElements(text, 3))}]");
+
+string[] GetArraySpecElements(string[] array, int len) //Метод возврвщает массив из элементов строкового массива array, длина которых меньше или равна заданной - len
+{
+    string[] result = new string[GetNumberSpecElements(array, len)];
+    int index = 0;
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        if (array[i].Length <= len)
+        {
+            result[index] = array[i];
+            index++;
+        }
+    }
+return result;
+}
+
+int GetNumberSpecElements(string[] array, int len) //Метод возврвщает количество элементов в строковом массиве array, длина которых меньше или равна заданной - len
+{
+int number = 0;
+for (int i = 0; i < array.Length-1; i++)
+{
+    if (array[i].Length <= len) number++;
+}
+return number;
+}
